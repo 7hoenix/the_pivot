d033b3ff33d363f58b41566029b680c87ab64639
philmphoenix <jpholzmann@gmail.com>
2015-10-13 16:22:04 -0600

    Write feature test for jobs

---
+++ spec/factories/user.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    display_name "Stanley"
+    full_name "Stanley Ski"
+    email "stanley@example.com"
+    password "password"
+  end
+end
---
+++ spec/features/user_sees_jobs_in_profile_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+def log_in
+  visit "/login"
+  click_on "Login"
+  fill_in("session[email]", with: @user.email)
+  fill_in("session[password]", with: @user.password)
+  click_on "Login"
+end
+
+RSpec.feature "A user" do
+  context "is logged in" do
+    before (:each) do
+      @user = create(:user)
+      log_in
+    end
+    scenario "and sees a feed of all jobs" do
+      job = create(:job)
+      job2 = create(:job)
+
+      expect(page).to have_content(job.title)
+      expect(page).to have_content(job2.title)
+    end
+  end
+end
