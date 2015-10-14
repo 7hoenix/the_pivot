d2767b67540ed09e07ffaaca7ef4cb5e91c98713
philmphoenix <jpholzmann@gmail.com>
2015-10-13 20:54:02 -0600

    Changes names of identifiers and methods
    Changes the names of identifiers and methods from items
    to jobs.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,21 +1,21 @@
 class CartController < ApplicationController
   def create
-    @current_cart.add_item(params[:item_id])
+    @current_cart.add_job(params[:job_id])
     session[:cart] = @current_cart.cart_items
     redirect_to cart_path
   end
 
   def delete
-    @current_cart.delete_item(params[:format])
+    @current_cart.delete_job(params[:format])
     redirect_to cart_path
   end
 
   def update
     if params[:quantity] == "increase"
-      @current_cart.increase_item_quantity(params[:item_id])
+      @current_cart.increase_job_quantity(params[:job_id])
       redirect_to cart_path
     elsif params[:quantity] == "decrease"
-      @current_cart.decrease_item_quantity(params[:item_id])
+      @current_cart.decrease_job_quantity(params[:job_id])
       redirect_to cart_path
     end
   end
