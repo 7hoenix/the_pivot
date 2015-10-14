e2976c29d6f47ec9e3d013cb7a5a296ee43c2b92
philmphoenix <jpholzmann@gmail.com>
2015-10-14 14:31:13 -0600

    Adds new action to jobs controller
    Our new jobs action used to be only nested within the admin
    namespace. This commit pulls it out for now but it will
    eventually need to be namespaced within the store admin
    namespace.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,4 +11,14 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def new
+    if current_user && current_user.has_business?
+      @job = Job.new
+    else
+      flash[:danger] = "Must have a business to register a new job
+      opportuntity"
+      redirect_to new_business_path
+    end
+  end
+
 end
