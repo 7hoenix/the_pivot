613f77e09c92b8420c53b30d5841754e5b164bef
philmphoenix <jpholzmann@gmail.com>
2015-10-13 20:59:05 -0600

    Changes this file to reflect jobs
    Got rid of one test whose functionality is out of scope
    and then changed the other two tests to reflect the new model
    name of jobs.

--- spec/features/unauthenticated_users_feature_spec.rb
+++ spec/features/unauthenticated_users_feature_spec.rb
@@ -3,35 +3,18 @@ require "rails_helper"
 RSpec.feature "Unauthenticated user browses items" do
 
   context "unauthenticated user" do
-    scenario "can browse the items" do
-      create(:item)
-      visit "/menu"
-      expect(current_path).to eq(menu_path)
-      expect(page).to have_content("Sushi")
+    scenario "can browse the jobs" do
+      create(:job)
+      visit "/jobs"
+      expect(current_path).to eq(jobs_path)
+      expect(page).to have_content("Jr dev")
     end
 
-    scenario "can browse items in each category" do
-      appetizers = Category.create(id: 1, title: "appetizers", description: "category")
-      item = create(:item)
-      item.categories = [appetizers]
-
-      visit "/categories/1"
-      expect(page).to have_content("Sushi")
-
-      sashimi = Category.create(id: 2, title: "sashimi", description: "category")
-      create(:item, title: "Hamachi", categories: [sashimi])
-
-      visit "/categories/2"
-      expect(page).to have_content("Hamachi")
-      expect(page).to_not have_content("Sushi")
-    end
-
-    scenario "can view an individual item" do
-      create(:item)
-      visit "/menu"
-      click_link_or_button "Sushi"
-      expect(page).to have_content("$11.00")
-      expect(page).to_not have_content("status")
+    scenario "can view an individual job" do
+      create(:job)
+      visit "/jobs"
+      click_on "Jr dev"
+      expect(page).to have_content("Write some code")
     end
   end
 end
