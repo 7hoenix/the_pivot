3c12a2abcb82746f59b7f456c921d8fc4c243871
philmphoenix <jpholzmann@gmail.com>
2015-10-14 14:32:16 -0600

    Adds has business method to user
    In order to verify if a user already has a business or not
    this commit adds a has business method to the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   validates :password, :full_name, :email, presence: true
   enum role: %w(user admin)
   has_many :orders
+
+  def has_business?
+    return true if current_user.business
+  end
 end
