120b62c682f1f264b0a59d0b1d05505a153fb7e9
philmphoenix <jpholzmann@gmail.com>
2015-10-14 14:34:34 -0600

    Adds business resource to routes new action
    Our first class citizen is businesses and in order to have them
    we needed to create a resource for them. Only adding one route
    at a time. This one starts the process with the new action.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :jobs, only: [:new, :show, :index]
   get "/jobs", to: "jobs#index"
 
+  resources :businesses, only: [:new]
+
   resources :categories do
     resources :items, only: [:show, :index]
   end
