38d60138cd6906b6a414a0928194aa60b9478681
philmphoenix <jpholzmann@gmail.com>
2015-10-14 14:58:36 -0600

    Forced a user to log in
    Users were being redirected to the new jobs path
    when they weren't logged in. This is wrong as in order
    to create a business you first need to be logged in.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -14,10 +14,13 @@ class JobsController < ApplicationController
   def new
     if current_user && current_user.has_business?
       @job = Job.new
-    else
+    elsif current_user
       flash[:danger] = "Must have a business to register a new job
       opportuntity"
       redirect_to new_business_path
+    else
+      flash[:danger] = "Log in to post a new job"
+      redirect_to login_path
     end
   end
 
