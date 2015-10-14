68935c8cda6dce17764d3e3ca07f30539c91f706
philmphoenix <jpholzmann@gmail.com>
2015-10-14 14:06:43 -0600

    Acceptance test for this card
    When this test passes this card is done.

---
+++ spec/features/user_creates_a_business_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+def log_in
+  visit "/login"
+  click_on "Login"
+  fill_in("session[email]", with: @user.email)
+  fill_in("session[password]", with: @user.password)
+  click_on "Login"
+end
+
+RSpec.feature "User creates a business" do
+  context "after logging in" do
+    before(:each) do
+      @user = create(:user)
+    end
+    scenario "and is redirected to their business dashboard" do
+      visit root_path
+      click_on "Post a Job"
+      expect(current_path).to eq(login_path)
+
+      log_in
+      expect(current_path).to eq(new_business_path)
+      fill_in("Name", with: "Pivotal Labs")
+      fill_in("About", with: "Business Consultancy")
+      click_on "Register Business"
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Pivotal Labs")
+      expect(page).to have_content("Business Consultancy")
+    end
+  end
+end
