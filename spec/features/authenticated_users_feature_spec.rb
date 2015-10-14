09e85921860a12ddf76c3b012bda5573fc81e631
HoffsMH <hecker.matthew@gmail.com>
2015-10-14 14:29:44 -0600

    fixed test in authenticated users feature test
    made sure that a user not already logged in cannot try to login again

--- spec/features/authenticated_users_feature_spec.rb
+++ spec/features/authenticated_users_feature_spec.rb
@@ -6,23 +6,20 @@ RSpec.feature "Authenticated user", type: :feature do
                              email: "stan@abc.com",
                              password: "password",
                              display_name: "Stan Smith")}
+    before(:each) do
+      visit '/login'
+      fill_in('session[email]', with: user.email)
+      fill_in('session[password]', with: user.password)
+      click_button 'Login'
+    end
 
     scenario "cannot log in" do
       visit '/login'
-      fill_in('session[email]', with: user.email)
-      fill_in('session[password]', with: user.password)
-      click_link_or_button 'Login'
-
-      expect(page).to_not have_content('Login')
-      expect(page).to have_content('Logout')
+      expect(page).to_not have_content('Please Log out First')
     end
 
     scenario "can log out" do
-      visit '/login'
-      fill_in('session[email]', with: user.email)
-      fill_in('session[password]', with: user.password)
-      click_link_or_button 'Login'
-      click_link_or_button 'Logout'
+      click_link_or_button 'Log out'
 
       expect(current_path).to eq(root_path)
       expect(@current_user).to eq(nil)
@@ -35,7 +32,7 @@ RSpec.feature "Authenticated user", type: :feature do
       click_link_or_button 'Login'
       visit '/admin'
 
-      expect(page).to_not have_content('admin') 
+      expect(page).to_not have_content('admin')
     end
   end
 end
