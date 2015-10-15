c192cc91d13514f29571d99cdd1495ab742baa92
philmphoenix <jpholzmann@gmail.com>
2015-10-14 19:07:34 -0600

    Creates a generate slug method
    Now we will not create a new business if the slug
    and therefore url is already taken in the database.
    This method will trigger right before a validation
    on the business model.

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,3 +1,13 @@
 class Business < ActiveRecord::Base
   belongs_to :user
+
+  validates :slug, uniqueness: true, presence: true
+
+  before_validation :generate_slug
+
+  private
+
+  def generate_slug
+    self.slug = name.parameterize
+  end
 end
