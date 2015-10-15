79691efd9b4899254c90f2d5f969a334236b5b23
philmphoenix <jpholzmann@gmail.com>
2015-10-15 15:24:56 -0600

    Adds full address method
    In order to display addresses easily we have
    implented a full address method to parse the
    address info into a single string.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ActiveRecord::Base
   belongs_to :addressable, polymorphic: true
+
+  def full_address
+    if unit
+      "#{street} #{unit} #{city}, #{state} #{zip}"
+    else
+      "#{street} #{city}, #{state} #{zip}"
+    end
+  end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#full_address" do
+    it "takes the elements of an address and puts them together" do
+      address = create(:address)
+
+      expect(address.full_address).to eq("1644 Platte st Denver, CO 80202")
+    end
+  end
 end
