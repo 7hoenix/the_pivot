4561ddf43f1befa33cee8c22fc24a45d7664f43e
philmphoenix <jpholzmann@gmail.com>
2015-10-15 14:27:46 -0600

    Comments out broken test
    Test is broken.

--- spec/models/watchlist_spec.rb
+++ spec/models/watchlist_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 
-RSpec.describe WatchList, type: :model do
+RSpec.describe Watchlist, type: :model do
   context "an job is added to the watchlist" do
     it "with valid attributes" do
       job = create(:job)
-      watchlist = WatchList.new(nil)
+      watchlist = Watchlist.new(nil)
       watchlist.add_job(job)
-      expect(watchlist.watchlist_items.count).to eq(1)
+      expect(watchlist.watchlist_jobs.count).to eq(1)
       job2 = Job.create
       watchlist.add_job(job2)
-      expect(watchlist.watchlist_items.count).to eq(2)
+      expect(watchlist.watchlist_jobs.count).to eq(2)
     end
   end
 end
