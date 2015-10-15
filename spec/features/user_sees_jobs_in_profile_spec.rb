595df90ab885a8faa276b027bbc3a0cf7398615e
HoffsMH <hecker.matthew@gmail.com>
2015-10-14 22:48:06 -0600

    final commit  before deploy prep
    all test passing
    pre assets are uncompiled at this point

--- spec/features/user_sees_jobs_in_profile_spec.rb
+++ spec/features/user_sees_jobs_in_profile_spec.rb
@@ -1,18 +1,11 @@
 require "rails_helper"
 
-def log_in
-  visit "/login"
-  fill_in("session[email]", with: @user.email)
-  fill_in("session[password]", with: @user.password)
-  click_button "Login"
-end
 
 RSpec.feature "A user" do
   context "is logged in" do
     before (:each) do
       @user = create(:user)
       visit "/login"
-      click_button "Login"
       fill_in("session[email]", with: @user.email)
       fill_in("session[password]", with: @user.password)
       click_button "Login"
@@ -29,7 +22,7 @@ RSpec.feature "A user" do
     scenario "and sees a feed of jobs on their 'watchlist'" do
       job = create(:job)
       job2 = create(:job)
-      log_in
+      visit "/profile"
 
       expect('.watchlist').not_to have_content(job.title)
       within(first('.job')) do
@@ -38,7 +31,6 @@ RSpec.feature "A user" do
       within(first(".job-listing-#{job2.title.parameterize}")) do
         click_button('Watch This Job')
       end
-      save_and_open_page
       within('.watchlist') do
         expect(page).to have_content(job.title)
       end
