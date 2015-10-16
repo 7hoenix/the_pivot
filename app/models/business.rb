72ea8604319b9fec462099265d93d5434b5a3ae4
philmphoenix <jpholzmann@gmail.com>
2015-10-15 18:29:27 -0600

    Adds polymorphic characteristics to business
    Now a business can have many different addresses.

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,6 @@
 class Business < ActiveRecord::Base
   belongs_to :user
+  has_many :addresses, as: :addressable
 
   validates :name, uniqueness: true, presence: true
   validates :about, presence: true
