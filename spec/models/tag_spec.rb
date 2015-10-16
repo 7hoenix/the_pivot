29b2dd5c403c245d790d142ee3edd42f41c63d46
Stanley <stanleyjsdz@gmail.com>
2015-10-15 20:56:30 -0600

    add validation spec
    added a spec to check validation for name
    
    added a tag factory

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,7 +2,20 @@ require "rails_helper"
 
 RSpec.describe Tag, type: :model do
   it "it exists" do 
-    tag = Tag.new
+    tag = create(:tag)
+
     expect(tag).to be_valid
+  end
+
+  context "valid attributes" do
+    it "is only valid with correct attributes" do
+      tag = Tag.new(name: "Ruby")
+      
+      expect(tag).to be_valid
+
+      tag.name = nil
+
+      expect(tag).to be_invalid
+    end
   end
 end
