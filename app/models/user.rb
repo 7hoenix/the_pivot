c288c84494433bb2ce5f148d6b4d244295d3d9d8
philmphoenix <jpholzmann@gmail.com>
2015-10-17 15:13:18 -0600

    Adds method and removes faulty dependencies
    Implements a find or create by oauth method that will try to
    find or create a new user and if so save it. Also removes
    old validations from bcrypt.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 class User < ActiveRecord::Base
-  has_secure_password
-  validates :password, :full_name, :email, presence: true
   enum role: %w(user admin)
   has_many :orders
   has_one :business
 
   def has_business?
     return true if business
+  end
+
+  def self.find_or_create_by_oauth(oauth)
+    user = User.find_or_create_by(provider: oauth.provider, uid: oauth.uid)
+
+    user.email = oauth.info.email
+    user.image_url = oauth.info.image
+    user.token = oauth.credentials.token
+    user.save
+
+    user
   end
 end
