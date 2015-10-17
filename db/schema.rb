b11dd1765b4db98f6987f954ff21439e63cd6c58
philmphoenix <jpholzmann@gmail.com>
2015-10-17 14:54:38 -0600

    Adds columns to users
    Adds in columns for github auth to users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015160052) do
+ActiveRecord::Schema.define(version: 20151017205150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,10 @@ ActiveRecord::Schema.define(version: 20151015160052) do
     t.text    "full_name"
     t.integer "role",            default: 0
     t.text    "display_name"
+    t.string  "provider"
+    t.string  "token"
+    t.string  "uid"
+    t.string  "image_url"
   end
 
   add_foreign_key "businesses", "users"
