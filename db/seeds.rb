cbe99edc1fa33c430838fc9f0d3c94d056bd68a8
HoffsMH <hecker.matthew@gmail.com>
2015-10-18 16:10:33 -0600

    seeds
    we now have seeds for businesses and jobs
    at some point we will have to clean up the tables that are no longer used in this project
    
    orders and categories for example

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,23 @@
 require "faker"
+
+def gen_address
+  Address.new(
+    street: Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
+    zip: Faker::Address.zip,
+  )
+end
+
+def random_job_status
+  chance = Random.rand(100)
+  if chance > 75
+    "retired"
+  else
+    "active"
+  end
+end
+
 tag_names = {
   php: "#6082BB",
   ruby: "#A61501",
@@ -23,9 +42,9 @@ demo_user = Random.rand(user_count)
 demo_admin = Random.rand(user_count)
 
 user_count.times do |index|
-  if index == demo_user 
+  if index == demo_user
     name = Faker::Name.name
-    User.create(
+    @demo_user = User.create(
       full_name: name,
       email: Faker::Internet.email(name),
       display_name: Faker::Internet.user_name(name),
@@ -33,11 +52,10 @@ user_count.times do |index|
       token: Faker::Bitcoin.address,
       uid: Faker::Code.ean,
       image_url: Faker::Avatar.image( Faker::Bitcoin.address,"50x50", "jpg"),
-      role: 4
     )
   elsif index == demo_admin
     name = Faker::Name.name
-    User.create(
+    @demo_admin = User.create(
       full_name: name,
       email: Faker::Internet.email(name),
       display_name: Faker::Internet.user_name(name),
@@ -45,11 +63,11 @@ user_count.times do |index|
       token: Faker::Bitcoin.address,
       uid: Faker::Code.ean,
       image_url: Faker::Avatar.image( Faker::Bitcoin.address,"50x50", "jpg"),
-      role: 4
+      role: 1
     )
   else
     name = Faker::Name.name
-    User.create(
+    user =   User.create(
       full_name: name,
       email: Faker::Internet.email(name),
       display_name: Faker::Internet.user_name(name),
@@ -58,5 +76,42 @@ user_count.times do |index|
       uid: Faker::Code.ean,
       image_url: Faker::Avatar.image( Faker::Bitcoin.address,"50x50", "jpg")
     )
+    user_tag_names = TagName.all.sample(Random.rand(TagName.all.count))
+    user_tag_names.each do |tag_name|
+      user.tags << Tag.new(tag_name_id: tag_name.id)
+    end
   end
+end
+
+
+#businesses
+puts "creating businesses..."
+business_count = 35
+user_list = User.all
+
+business_count.times do |index|
+  business = Business.new(
+    name: Faker::Company.name,
+    about: Faker::Company.catch_phrase,
+  )
+  Random.rand(4).times do
+    business.addresses <<  gen_address
+  end
+  business.user = user_list.shuffle.pop
+  business.save
+end
+
+#Jobs
+puts "creating jobs..."
+jobs_count = 300
+
+jobs_count.times do |index|
+  job = Job.new(
+    title: Faker::Name.title,
+    description: Faker::Lorem.paragraphs(Random.rand(3)),
+    benefits: Faker::Lorem.paragraphs(Random.rand(1)),
+    status: random_job_status
+  )
+  job.business = Business.all.sample
+  job.save
 end
