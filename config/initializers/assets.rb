32ee510571df9111db21241088c68b29f8efe87a
philmphoenix <jpholzmann@gmail.com>
2015-10-18 01:54:52 -0600

    Implments Javascript for clicking on links.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( business_dashboard.js )
+Rails.application.config.assets.precompile += %w( profile.js business_dashboard.js )
