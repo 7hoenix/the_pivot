0e02dfcd4239c84962e413de1ef7ad51935f9d31
philmphoenix <jpholzmann@gmail.com>
2015-10-17 22:19:57 -0600

    Adds a create method to create tags and tagnames
    Now an admin can create a tag which automattically creates
    an associated tagname.

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -3,4 +3,26 @@ class Admin::TagsController < AdminController
     @tag = Tag.new
     @tag_name = TagName.new
   end
+
+  def create
+    tag = Tag.find_or_create_by(tag_params)
+    if tag.save
+      tag_name = tag.tag_name = TagName.find_or_create_by(tag_name_params)
+      tag_name.save
+      redirect_to admin_path
+    else
+      flash[:errors] = "Tag not created"
+      render :new
+    end
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:tag_name_id)
+  end
+
+  def tag_name_params
+    params[:tag].require(:tag_name).permit(:name)
+  end
 end
