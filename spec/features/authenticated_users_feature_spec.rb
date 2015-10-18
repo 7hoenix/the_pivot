9717b4a56b7d1c6657b70e9e9782d4db4bf4213b
HoffsMH <hecker.matthew@gmail.com>
2015-10-18 17:07:16 -0600

    fixed user test
    test  for logging out in authenticated users feature spec now works and
    include the mocked github login

--- spec/features/authenticated_users_feature_spec.rb
+++ spec/features/authenticated_users_feature_spec.rb
@@ -1,16 +1,14 @@
 require "rails_helper"
 
 RSpec.feature "Authenticated user", type: :feature do
+  include IntegrationSpecHelper
   context "is logged in" do
 
-    # this test belongs as a controller test
-    # scenario "cannot log in" do
-    #   visit '/login'
-    #   expect(page).to have_content('Please Log out First')
-    # end
-
     scenario "can log out" do
+      login_with_oauth
+      save_and_open_page
       click_link_or_button 'Log out'
+
 
       expect(current_path).to eq(root_path)
       expect(@current_user).to eq(nil)
