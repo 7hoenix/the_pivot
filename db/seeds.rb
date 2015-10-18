64d8861b9bd24a8a62842361c982c092185bd5a8
philmphoenix <jpholzmann@gmail.com>
2015-10-18 02:23:14 -0600

    Comments out users in seeds file for github auth
    We are now using github auth for logging in a new user.
    Our old user ways of doing things does not work.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,11 @@
 
 # users
 
-User.create(full_name: "Rachel Warbelow", email: "demo+rachel@jumpstartlab.com" , display_name: nil, password: "password", role: 0)
-User.create(full_name: "Jeff Casimir", email: "demo+jeff@jumpstartlab.com", display_name: "j3", password: "password", role: 0)
-User.create(full_name: "Jorge Tellez", email: "demo+jorge@jumpstartlab.com", display_name: "novohispano", password: "password", role: 0)
-User.create(full_name: "Josh Cheek", email: "demo+josh@jumpstartlab.com", display_name: "josh", password: "password", role: 1)
-User.create(full_name: 'D G', email: "dg", display_name: 'Synergy', password: 'dg', role: 1)
-
+# User.create(full_name: "Rachel Warbelow", email: "demo+rachel@jumpstartlab.com" , display_name: nil, password: "password", role: 0)
+# User.create(full_name: "Jeff Casimir", email: "demo+jeff@jumpstartlab.com", display_name: "j3", password: "password", role: 0)
+# User.create(full_name: "Jorge Tellez", email: "demo+jorge@jumpstartlab.com", display_name: "novohispano", password: "password", role: 0)
+# User.create(full_name: "Josh Cheek", email: "demo+josh@jumpstartlab.com", display_name: "josh", password: "password", role: 1)
+# User.create(full_name: 'D G', email: "dg", display_name: 'Synergy', password: 'dg', role: 1)
 
 
 Job.create(title: "senior rails dev", description: "all the backend stuff", business_id: 2, address_id: 4)
