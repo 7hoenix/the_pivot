363ee2a74536a87b333208b1f3741d6f62f5341d
HoffsMH <hecker.matthew@gmail.com>
2015-10-18 18:01:02 -0600

    fixed misspelling of taggable in model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-  validates :tag_name_id, :tagable_id, :tagable_type, presence: true
+  validates :tag_name_id, :taggable_id, :taggable_type, presence: true
   validates :tag_name_id, presence: true
   belongs_to :taggable, polymorphic: true
   belongs_to :tag_name
