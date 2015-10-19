cf4ad0084bd2a13646efc8dcbf5f38b66ebe1c34
HoffsMH <hecker.matthew@gmail.com>
2015-10-18 20:40:00 -0600

    location
    location is now added to users and is pulled from github upon sigining in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019020232) do
+ActiveRecord::Schema.define(version: 20151019023319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20151019020232) do
     t.string  "image_url"
     t.string  "nickname"
     t.string  "github"
+    t.string  "location"
   end
 
   create_table "watched_jobs", force: :cascade do |t|
