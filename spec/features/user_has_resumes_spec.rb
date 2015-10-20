8885b1e8a061e30c2cb8b75427841ea800ea93fc
philmphoenix <jpholzmann@gmail.com>
2015-10-20 16:34:40 -0600

    Fixes test for user has resume
    This is the the acceptance test for when users should have
    resumes.

--- spec/features/user_has_resumes_spec.rb
+++ spec/features/user_has_resumes_spec.rb
@@ -3,17 +3,21 @@ require "rails_helper"
 RSpec.feature "User has resumes" do
   context "to choose from" do
     xscenario "after creating them" do
-      @user = create(:user)
-      session[:user_id] = @user.id
+      login_with_oauth
 
       visit profile_path
       click_on("Create New Resume")
-      fill_in("Title", with: "Ruby Programmer")
+      fill_in("Title", with: "Ruby Developer")
       select("Repo1", from: "Repositories")
+      fill_in("Additional Information", with: "My first project")
       click_on("Create Resume")
 
       expect(current_path).to eq(profile_path)
       expect(page).to have_content("Ruby Programmer")
+
+      click_on("Ruby Programmer")
+      expect(current_path).to eq(resume_path(Resume.last))
+      expect(page).to have_content("My first project")
     end
   end
 end
