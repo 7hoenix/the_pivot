25e89ce737288ef431cae3450cd3c9426e53c060
philmphoenix <jpholzmann@gmail.com>
2015-10-21 14:39:14 -0600

    Adds rerouting method if coming from job path
    Now if a user hits the create route from the job path
    they are redirected there after filling out a resume.

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -6,13 +6,13 @@ class ResumesController < ApplicationController
 
   def create
     resume = current_user.resumes.new(resume_params)
+    if resume.save
     resume_params[:repository_ids].each do |repository_id|
       ResumeRepository.find_or_create_by(resume_id: resume.id, repository_id:
         repository_id)
     end
-    if resume.save
       flash[:success] = "Resume created"
-      redirect_to profile_path
+      redirect_back_or profile_path
     else
       flash[:errors] = "Resume not created"
       render :new
