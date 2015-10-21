02d513c09ba8a382470dc150d534daefe52f7b0f
philmphoenix <jpholzmann@gmail.com>
2015-10-20 22:13:18 -0600

    Adds resume controller with new create and show actions
    This is the full implementation that should be read along with this
    entire pr. It uses a poro to populate new repository objects and
    then pass those objects down to the new resume view. In the create
    action it places both resume id and repository id in the resume
    repository join table.

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,6 +1,38 @@
 class ResumesController < ApplicationController
   def new
     @resume = Resume.new
-    @repositories = current_client.reposoitories(current_user.nickname).map { |repo| repo[:name] }
+    handler ||= RepositoryHandler.new(current_client, current_user)
+    if handler.populate_repositories
+      @repositories = Repository.all
+    else
+      flash[:errors] = "Couldn't hear back from github"
+      redirect_to profile_path
+    end
+  end
+
+  def create
+    resume = current_user.resumes.new(resume_params)
+    resume_params[:repository_ids].each do |repository_id|
+      ResumeRepository.find_or_create_by(resume_id: resume.id, repository_id:
+        repository_id)
+    end
+    if resume.save
+      flash[:success] = "Resume created"
+      redirect_to profile_path
+    else
+      flash[:errors] = "Resume not created"
+      render :new
+    end
+  end
+
+  def show
+    @resume = Resume.find(params[:id])
+    @repositories = @resume.repositories
+  end
+
+  private
+
+  def resume_params
+    params.require(:resume).permit(:title, repository_ids: [])
   end
 end
