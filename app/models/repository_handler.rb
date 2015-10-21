e9bad0d52c15f6e539d737d4592ec4c7d861d40b
philmphoenix <jpholzmann@gmail.com>
2015-10-20 22:17:21 -0600

    Poro to handle the creation of repositories
    In order to make sure that we only call the github api
    one time, and to try to clean up the controller we created
    a new poro that handles the creation of repositories from
    the github api.

---
+++ app/models/repository_handler.rb
@@ -0,0 +1,15 @@
+class RepositoryHandler
+  attr_reader :client, :user
+
+  def initialize(current_client, current_user)
+    @client = current_client
+    @user   = current_user
+  end
+
+  def populate_repositories
+    client.repositories(user.nickname).each do |repo|
+      Repository.create(full_name: repo[:full_name], name: repo[:name])
+    end
+    true
+  end
+end
