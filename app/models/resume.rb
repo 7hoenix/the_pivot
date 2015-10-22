b45fa3eae24d9a71e139649ee893e7792071f5a0
Stanley <stanleyjsdz@gmail.com>
2015-10-21 21:51:05 -0600

    integrate cover_letter into resume
    change resume new and show views to accomodate for cover letter,
    change model and controller to accomodate for cover letter via paperclip

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,5 +1,7 @@
 class Resume < ActiveRecord::Base
   has_many :resume_repositories
   has_many :repositories, through: :resume_repositories
+  has_attached_file :cover_letter, storage: :s3, s3_credentials: {:bucket => ENV["bucket"], :access_key_id => ENV["aws_key_id"], :secret_access_key => ENV["aws_secret_key"]}
+  validates_attachment_content_type :cover_letter, content_type: /\Aapplication\/.*\Z/
   belongs_to :user
 end
