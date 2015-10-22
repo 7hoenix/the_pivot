558baa6e62b1962fef56ebe5da6392a1c8e3e6de
Stanley <stanleyjsdz@gmail.com>
2015-10-21 21:45:07 -0600

    change schema for paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021022125) do
+ActiveRecord::Schema.define(version: 20151021201625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,8 +83,12 @@ ActiveRecord::Schema.define(version: 20151021022125) do
   end
 
   create_table "resumes", force: :cascade do |t|
-    t.integer "user_id"
-    t.string  "title"
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "cover_letter_file_name"
+    t.string   "cover_letter_content_type"
+    t.integer  "cover_letter_file_size"
+    t.datetime "cover_letter_updated_at"
   end
 
   create_table "tag_names", force: :cascade do |t|
